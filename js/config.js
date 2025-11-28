914b70475872991193d076d1f7c7cdc9cf256c6b
rahulop2003 <157278822+rahulop2003@users.noreply.github.com>
2025-11-28 15:08:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Yj9pCJetO/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
